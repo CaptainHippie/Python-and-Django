42ab021fc8e88cfbdd213ddab9c9c120c6122d4b
Lousy_malayali <neerajvb@gmail.com>
2022-05-17 08:25:11 +0530

    3

--- Django/reglogin/website/urls.py
+++ Django/reglogin/website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_req,name='reg'),
-    path('login/',views.login_req,name='log'),
-    path('dashboard/',views.logout,name='logout')
+    path('register/', views.register_req, name='reg'),
+    path('login/', views.login_req, name='log'),
+    path('dashboard/', views.logout, name='logout')
 ]
